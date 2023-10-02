be1e204cde95b34eca570da108706117913b9929
Xavier Aibar Martínez <xavaibmar@alu.edu.gva.es>
2023-10-02 20:24:41 +0200

    Actividad 3 completada

---
+++ UD2/actividades3/ej1.js
@@ -0,0 +1,6 @@
+let r1 = Math.random() * 256;
+let r2 = Math.random() * 256;
+let r3 = Math.random() * 256;
+
+document.addEventListener("DOMContentLoaded", () => {document.body.style.backgroundColor 
+    = "rgb(" + r1 + ", " + r2 + ", " + r3 + ")"});
